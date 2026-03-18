885583daace16ef7621f4865167510f18f8e7290
Aarushi S <connect2aarushi@gmail.com>
2026-03-18 09:38:18 +0530

    feat: added pydantic schemas and password hashing

---
+++ app/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+
+# Defining what user must send to register
+class UserCreate(BaseModel):
+    full_name: str
+    email: EmailStr
+    password: str
+    branch: str
+    batch: str
+
+# Data going OUT
+class UserOut(BaseModel):
+    id : int
+    full_name: str
+    email: EmailStr
+    branch: str
+    batch:str
+    is_verified: bool
+    class Config:
+        from_attributes = True
