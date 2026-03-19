0a560fd61a37662eec153ba2e03eaef8eca3289c
Aarushi S <connect2aarushi@gmail.com>
2026-03-19 16:45:05 +0530

    feat: implemented JWT token generation and security config. Updated requirements.txt

--- app/schemas.py
+++ app/schemas.py
@@ -19,3 +19,10 @@ class UserOut(BaseModel):
     is_verified: bool
     class Config:
         from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: str | None = None
--- app/utils/security.py
+++ app/utils/security.py
@@ -1,4 +1,14 @@
 from passlib.context import CryptContext
+import os
+from datetime import datetime,timedelta,timezone
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+
+
+# Secret key from .env
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 # Password hashing
 pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
@@ -6,3 +16,13 @@ def hash_password(password:str) -> str:
     return pwd_context.hash(password)
 def verify_password(plain_password:str, hashed_password:str) -> bool:
     return pwd_context.verify(plain_password,hashed_password)
+
+# Token creation
+def create_access_token(data:dict):
+    to_encode = data.copy()
+    # Setting expiration time (current time + 30 mins)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    #Signing the token with the Secret Key
+    encoded_jwt = jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+    return encoded_jwt
